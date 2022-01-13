cf93e05d26e5f0a468064bbbf12236ecb6d53d4c
Emily Burger <eburger939@gmail.com>
2022-01-13 15:40:51 -0600

    heroku working changed start/dev

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/api', api)
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
-  });
+  })
 
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
