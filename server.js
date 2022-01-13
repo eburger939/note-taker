b96deb32e3252feb3380d7a921238114237a9d60
Emily Burger <eburger939@gmail.com>
2022-01-13 13:59:41 -0600

    added /

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
   });
 
   app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
   });
 
   app.get('*', (req, res) => {
@@ -28,3 +28,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 });
+
